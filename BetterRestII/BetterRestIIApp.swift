80f10f9cef16b659e3014fe444f2d453fe386a61
Caio Castro <caio.castro3909@gmail.com>
2022-08-18 12:36:42 +0200

    Initial Commit

---
+++ BetterRestII/BetterRestIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestIIApp.swift
+//  BetterRestII
+//
+//  Created by Caio Castro on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
